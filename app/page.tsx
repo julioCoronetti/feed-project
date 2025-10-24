b8f8ab687a19f6b8316a4d5a41c6aa3e12166811
Julio Coronetti <timeset13@outlook.com>
2025-10-23 22:19:38 -0300

    Implement Profile and Card components; update package dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <h1>Feed</h1>;
-}
+import { Profile } from "@/components/Profile";
+import { Card } from "@/components/ui/card";
+
+const App = () => {
+  return (
+    <div className="w-full h-screen flex justify-center gap-15 pt-30">
+      <Profile />
+      <Card className="w-150 h-full bg-accent rounded-b-none"></Card>
+    </div>
+  );
+};
+
+export default App;
---
+++ components/Profile.tsx
@@ -0,0 +1,5 @@
+import { Card } from "./ui/card";
+
+export const Profile = () => {
+  return <Card className="w-70 h-60 bg-accent"></Card>;
+};
