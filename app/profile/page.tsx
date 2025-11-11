790a4283656d86db2d1f891b7a390d3b9128e9d3
Julio Coronetti <timeset13@outlook.com>
2025-11-10 21:10:45 -0300

    Refactor Profile component layout and styles; add Separator component for improved UI structure; update package dependencies

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,21 +1,19 @@
 import { Card } from "@/components/ui/card";
 import { ProfileCardUser } from "./components/profile";
+import { Separator } from "@/components/ui/separator";
+import { Settings } from "lucide-react";
 
 const Profile = () => {
   return (
     <div className="w-full flex flex-col gap-5 items-center">
       <h1 className="text-3xl font-medium w-160">Profile</h1>
       <div className="w-160 flex justify-between">
-        <Card className="w-60 h-130 overflow-hidden">
-          <p className="h-10 flex justify-center items-center">
-            Recommendations
-          </p>
-          <div className="w-full h-20 border-t-2 border-foreground"></div>
-          <div className="w-full h-20 border-t-2 border-foreground"></div>
-          <div className="w-full h-20 border-t-2 border-foreground"></div>
-          <div className="w-full h-20 border-t-2 border-foreground"></div>
-          <div className="w-full h-20 border-t-2 border-foreground"></div>
-          <div className="w-full h-20 border-t-2 border-foreground"></div>
+        <Card className="w-60 h-130 overflow-hidden py-2 px-4">
+          <h2 className="h-10 flex items-center font-medium gap-2">
+            <Settings />
+            Settings
+          </h2>
+          <Separator className="bg-accent-foreground" />
         </Card>
         <ProfileCardUser />
       </div>
