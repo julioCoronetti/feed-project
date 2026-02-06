16d91d0ecbfde6171c108f940702760baf1dc24c
Julio Coronetti <timeset13@outlook.com>
2026-02-06 21:04:47 +0000

    feat: adicionar sombra ao componente principal para melhor destaque

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <main className="w-180 h-[calc(100vh-7rem)] border-2 border-b-transparent border-foreground rounded-b-none rounded-2xl"></main>
+    <main className="w-180 h-[calc(100vh-7rem)] border-2 border-b-transparent border-foreground rounded-b-none rounded-2xl drop-shadow-md"></main>
   );
 };
 
